c4e0962529cd3cd5d5f205839563c221540d5de5
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2022-10-12 14:00:30 +0300

    Player Script is updated

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,11 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         //Move the vehicle to forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
